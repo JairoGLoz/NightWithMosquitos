321b39db196eeafba09be46cd844dae0fa2fcb2b
Jairo Lozano <jglozano@blacklabs.mx>
2017-11-10 18:21:46 -0700

    Fireplace added, shooting script added

--- Assets/Script/PlayerArmController.cs
+++ Assets/Script/PlayerArmController.cs
@@ -1,20 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using VRStandardAssets.Utils;
 
 public class PlayerArmController : MonoBehaviour {
 
     public Image reticle;
+    public VRInput m_VRInput;
+    public VREyeRaycaster m_EyeRaycaster;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(reticle.transform.position);
+
+    private void OnEnable()
+    {
+        m_VRInput.OnDown += HandleDown;
+    }// end of OnEnable
+
+    private void HandleDown()
+    {
+        // if there is an interactible currently being looked at
+        MosquitoController shootingMosquito = m_EyeRaycaster.CurrentInteractible ?
+            m_EyeRaycaster.CurrentInteractible.GetComponent<MosquitoController>() : null;
+
+        // get target transform
+        Transform target = shootingMosquito ? shootingMosquito.transform : null;
+
+        // Start shooting coroutine
+        StartCoroutine(Fire(target));
+    }// end of HandleDown
+
+    private IEnumerator Fire(Transform target)
+    {
+        // Play sound
+
+        // show fire
+
+        // Hit mosquito
+
+        yield return null;
+    }// end of fire
+
+    // Update is called once per frame
+    void Update () {
         transform.LookAt(reticle.transform.position);
 	}
 }
